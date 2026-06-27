f69fbe4f9f5408958673d6e444d47fe0f18488e5
Bhavay Malhotra <rockwizard2001@gmail.com>
2026-06-27 05:22:38 -0700

    Download all necessary models

--- models_download.py
+++ models_download.py
@@ -23,17 +23,25 @@ print(f"Routing all Hugging Face downloads to: {os.environ['HF_HOME']}\n")
 # =====================================================================
 models_to_download = [
     {
-        "repo_id": "Qwen/Qwen3-30B-A3B-Instruct-2507",
-        "local_dir": "./Qwen3-30B-A3B-Instruct"
+        "repo_id": "meta-llama/Llama-Guard-3-8B",
+        "local_dir": "./Meta-Llama-Guard-3-8B"
     },
     {
-        "repo_id": "allenai/wildguard",
-        "local_dir": "./allenai-wildguard-7B"
+        "repo_id": "meta-llama/Meta-Llama-3.1-8B-Instruct",
+        "local_dir": "./Llama-3-8B-Instruct"
     },
-    # {
-    #     "repo_id": "google/shieldgemma-9b",
-    #     "local_dir": "./google-shieldgemma-9B"
-    # },
+    {
+        "repo_id": "mistralai/Mistral-7B-Instruct-v0.3",
+        "local_dir": "./Mistral-7B-Instruct-V0.3"
+    },
+    {
+        "repo_id": "google/gemma-7b-it",
+        "local_dir": "./Gemma-7B-It"
+    },
+    {
+        "repo_id": "google/shieldgemma-9b",
+        "local_dir": "./google-shieldgemma-9B"
+    },
 ]
 
 # =====================================================================
